f9acfaefd8f3bb2e045ef06a9cacf3944c654cac
Roman <Roman@DESKTOP-RS415KS>
2023-03-27 17:10:08 -0600

    Proyecto de emperesa WifiMex.

---
+++ SGMDT_Wifimex/Datos/Conexion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        public static MySqlConnection conexion;
+        public static MySqlTransaction transaccion;
+        static string usuario = "root";
+        static string password = "12345";
+        static string bd = "DBWifiMex";
+        static string servidor = "localhost";
+        static string puerto = "3306";
+
+        public static bool conectar()
+        {
+            try
+            {
+                conexion = new MySqlConnection("Database=" + bd + ";Data Source=" + servidor
+                    + ";Port=" + puerto + ";User Id=" + usuario + ";Password=" + password);
+
+                conexion.Open();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public static void desconectar()
+        {
+            try
+            {
+                conexion.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ SGMDT_Wifimex/Modelos/Empleados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Empleados
+    {
+        public string idempleado { get; set; }
+        public string nombrecompleto { get; set; }
+        public string RFC { get; set; }
+        public string CURP { get; set; }
+        public int Edad { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string fechaContratacion { get; set; }
+        public string Rol { get; set; }
+        public bool Estatus { get; set; }
+    }
+}
